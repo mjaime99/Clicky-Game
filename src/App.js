22a8d726de906360d15babc7b1a7ae299e012e24
unknown <mike_jaime@live.com>
2018-12-18 21:32:09 -0700

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,105 @@
 import React, { Component } from "react";
 import BandCard from "./components/BandCard";
+import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
 import bands from "./bands.json";
-import HelloNav from "./components/NavBar/HelloNav";
-import Counter from "./components/Counter";
+import "./App.css";
+
+function shuffleBands(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 class App extends Component {
 
   state = {
-    bands
+    bands,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
   };
 
-  removeBand = id => {
-
-    const bands = this.state.bands.filter(band => band.id !== id);
-
-    this.setState({ bands });
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
   };
 
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "MOSH ON!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "GUILLOTINE!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledBands = shuffleBands(bands);
+    this.setState({ bands: shuffledBands });
+  };
 
   render() {
     return (
       <Wrapper>
-        <HelloNav />
-        {this.state.bands.map(band => (
-          <BandCard
-            removeBand={this.removeBand}
-            id={band.id}
-            key={band.id}
-            name={band.name}
-            image={band.image}
-            genre={band.genre}
-            origin={band.origin}
-          />
-        ))}
+        <Nav
+          title="Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title>
+          jam each band, but only once... OR OFF WITH YOUR HEAD!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.bands.map(band => (
+              <Column size="md-3 sm-6">
+                <BandCard
+                  key={band.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={band.id}
+                  image={band.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
       </Wrapper>
     );
   }
-
-  Counter() {
-    return <Counter />;
-  }
-
 }
 
 export default App;
--- src/components/BandCard/index.js
+++ src/components/BandCard/index.js
@@ -1,18 +1,1 @@
-import React from "react";
-import "./style.css";
-
-function BandCard(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-        </div>
-      <div className="content" class="row justify-content-center">
-        <button type="button" class="btn btn-danger"><span onClick={() => props.removeBand(props.id)} className="remove">Click Here!</span></button>
-      </div>
-    </div>
-    
-  );
-}
-
-export default BandCard;
+export { default } from "./BandCard";
